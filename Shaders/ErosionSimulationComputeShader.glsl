dc89136675ce3f654b12087dce2b2227fb94c7b7
alexsaake <alexandersaake@googlemail.com>
2024-12-28 11:21:45 +0100

    Added cascade from https://github.com/erosiv/soillib/blob/main/source/particle/cascade.hpp

--- Shaders/ErosionSimulationComputeShader.glsl
+++ Shaders/ErosionSimulationComputeShader.glsl
@@ -60,12 +60,100 @@ const float EvaporationRate = 0.001;
 const float DepositionRate = 0.05;
 const float MinimumVolume = 0.001;
 const float Gravity = 2.0;
+const float MaxDiff = 0.8;
+const float Settling = 1.0;
 vec2 myPosition;
 vec2 myOriginalPosition;
 vec2 mySpeed;
 int myAge;
 float myVolume;
 float mySediment;
+
+//https://github.com/erosiv/soillib/blob/main/source/particle/cascade.hpp
+void Cascade(ivec2 ipos)
+{
+    if(isOutOfBounds(ipos))
+    {
+        return;
+    }
+
+    // Get Non-Out-of-Bounds Neighbors
+
+    const ivec2 n[] = {
+        ivec2(-1, -1),
+        ivec2(-1, 0),
+        ivec2(-1, 1),
+        ivec2(0, -1),
+        ivec2(0, 1),
+        ivec2(1, -1),
+        ivec2(1, 0),
+        ivec2(1, 1)
+    };
+
+    struct Point {
+        ivec2 pos;
+        float h;
+        float d;
+    } sn[8];
+
+    int num = 0;
+
+    for(int i = 0; i < n.length(); i++)
+    {
+        ivec2 nn = n[i];
+        ivec2 npos = ipos + nn;
+
+        if(isOutOfBounds(npos))
+        {
+            continue;
+        }
+
+        float height = heightMap[getIndexV(npos)];
+        sn[num].pos = npos;
+        sn[num].h = height;
+        sn[num].d = length(nn);
+        num++;
+    }
+
+    // Local Matrix, Target Height
+
+    float height = heightMap[getIndexV(ipos)];
+    float h_ave = height;
+    for(int i = 0; i < num; ++i)
+    {
+        h_ave += sn[i].h;
+    }
+    h_ave /= float(num + 1);
+
+    for (int i = 0; i < num; ++i)
+    {
+        // Full Height-Different Between Positions!
+        float diff = h_ave - sn[i].h;
+        if (diff == 0)
+        {
+            continue;
+        }
+
+        ivec2 tpos = (diff > 0) ? ipos : sn[i].pos;
+        ivec2 bpos = (diff > 0) ? sn[i].pos : ipos;
+
+        uint tindex = getIndexV(tpos);
+        uint bindex = getIndexV(bpos);
+
+        // The Amount of Excess Difference!
+        float excess = 0.0f;
+        excess = abs(diff) - sn[i].d * MaxDiff;
+        if (excess <= 0)
+        {
+            continue;
+        }
+
+        // Actual Amount Transferred
+        float transfer = Settling * excess / 2.0f;
+        heightMap[tindex] -= transfer;
+        heightMap[bindex] += transfer;
+    }
+}
 
 bool Move()
 {
@@ -84,7 +172,7 @@ bool Move()
     if(myVolume < MinimumVolume)
     {
         heightMap[getIndexV(position)] += mySediment;
-        //cascade(position);
+        Cascade(position);
         return false;
     }
 
@@ -150,7 +238,7 @@ bool Interact()
 
     myVolume *= (1.0 - EvaporationRate);
 
-    //cascade(position);
+    Cascade(position);
 
     myAge++;
 
