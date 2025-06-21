80b1ae293742f54caadd7519342aae6cd8df7257
Alexander Saake <alexandersaake@googlemail.com>
2025-06-21 12:04:42 +0200

    Reworked cliff heightmap generation.

--- ProceduralLandscapeGeneration/MapGeneration/GPU/Shaders/GenerateCoastlineCliffHeightMapComputeShader.glsl
+++ ProceduralLandscapeGeneration/MapGeneration/GPU/Shaders/GenerateCoastlineCliffHeightMapComputeShader.glsl
@@ -23,28 +23,34 @@ layout(std430, binding = 5) readonly restrict buffer mapGenerationConfigurationS
     MapGenerationConfiguration mapGenerationConfiguration;
 };
 
+uint myHeightMapPlaneSize;
+
+uint HeightMapLayerOffset(uint layer)
+{
+    return (layer * mapGenerationConfiguration.RockTypeCount + layer) * myHeightMapPlaneSize;
+}
+
 void main()
 {
     uint index = gl_GlobalInvocationID.x;
-    uint heightMapPlaneSize = heightMap.length() / (mapGenerationConfiguration.RockTypeCount * mapGenerationConfiguration.LayerCount + mapGenerationConfiguration.LayerCount - 1);
-    if(index >= heightMapPlaneSize)
+    myHeightMapPlaneSize = heightMap.length() / (mapGenerationConfiguration.RockTypeCount * mapGenerationConfiguration.LayerCount + mapGenerationConfiguration.LayerCount - 1);
+    if(index >= myHeightMapPlaneSize)
     {
         return;
     }
-    uint heightMapSideLength = uint(sqrt(heightMapPlaneSize));
+    uint heightMapSideLength = uint(sqrt(myHeightMapPlaneSize));
 
     uint x = index % heightMapSideLength;
     uint y = index / heightMapSideLength;
     
-    float bedrockHeight = 0.0;
-    float coarseSedimentHeight = 0.0;
+    float bedrockLayerOneHeight = 0.0;
     if((y >= heightMapSideLength / 8 * 3
         && x < heightMapSideLength / 2)
         || (y >= heightMapSideLength / 4
         && y < heightMapSideLength / 8 * 3
         && x < heightMapSideLength / 4 * 3))
     {
-        bedrockHeight = 1.0;
+        bedrockLayerOneHeight = 1.0;
     }
     if(mapGenerationConfiguration.LayerCount > 1
         && mapGenerationConfiguration.RockTypeCount > 1
@@ -53,10 +59,17 @@ void main()
         && x >= heightMapSideLength / 2
         && x < heightMapSideLength / 8 * 5)
     {
-        bedrockHeight = 0.0;
-        coarseSedimentHeight = 1.0;
-        heightMap[index + 1 * heightMapPlaneSize] = coarseSedimentHeight;
+        uint rockType = 1;
+        uint layer = 1;
+        bedrockLayerOneHeight = 0.0;
+        float coarseRockTypeLayerOneHeight = 0.5;
+        heightMap[index + rockType * myHeightMapPlaneSize] = coarseRockTypeLayerOneHeight;
+        
+        float bedrockLayerTwoHeight = 0.5;
+        float layerTwoFloorHeight = 0.5;
+        heightMap[index + HeightMapLayerOffset(layer)] = bedrockLayerTwoHeight;
+        heightMap[index + layer * mapGenerationConfiguration.RockTypeCount * myHeightMapPlaneSize] = layerTwoFloorHeight;
     }
 
-    heightMap[index] = bedrockHeight;
+    heightMap[index] = bedrockLayerOneHeight;
 }
